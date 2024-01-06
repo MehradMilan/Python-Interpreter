16ac7fcc9eaaad2135003190a83ceb0141f1320a
Mehrad <mehrad_milanloo@yahoo.com>
2024-01-06 15:58:59 +0330

    Init: added init files

---
+++ README.md
@@ -0,0 +1,3 @@
+# PythonMinus Interpreter
+### The `Programming Languages Design` course project.
+Fall 2023 - Sharif Universuty of Technology
---
+++ interpreter.rkt
@@ -0,0 +1,8 @@
+; Group members:
+;   1.  Ali Rahmizad: 99
+;   2.  Ali Safarafard: 99
+;   3.  Mehrad Milanloo: 99105775
+
+#lang racket
+
+(require (lib "eopl.ss" "eopl"))
