c69ed3e63a394ff371534653110c5daa5371c868
Ali Rahmizad <79265219+AliAgha888@users.noreply.github.com>
2024-01-11 15:37:52 +0330

    Adding all datatypes of the given grammar to datatypes.rkt

--- datatypes.rkt
+++ datatypes.rkt
@@ -1,47 +1,235 @@
 #lang racket
 (require (lib "eopl.ss" "eopl"))
 
+(define-datatype program program?
+  (prog
+   (program statements?)))
+
+(define-datatype statements statements?
+  (stmt
+   (stmt statement?))
+  (stmts
+   (stmts statements?)
+   (stmt statement?)))
+
 (define-datatype statement statement?
-  (assign (var string?) (expr expression?))
-  (global (var string?))
-  (return (expr expression?))
-  (return_void)
-  (pass)
-  (break)
-  (continue)
-  (func (name string?) (params func_param*?) (statements list?))
-  (if_stmt (cond_exp expression?) (if_sts list?) (else_sts list?))
-  (for_stmt (iter string?) (list_exp expression?) (sts list?))
-  (print_stmt (expressions expression*?))
-  )
+  (stmt-compound-stmt
+   (stmt compound-stmt?))
+  (stmt-simple-stmt
+   (stmt simple-stmt?)))
 
-(define-datatype func_param func_param?
-  (with_default (var string?) (expr expression?))
-  )
+(define-datatype simple-stmt simple-stmt?
+  (simple-stmt-assignment
+   (assignment assignments?))
+  (simple-stmt-global-stmt
+   (stmt global-stmt?))
+  (simple-stmt-return-stmt
+   (stmt return-stmt?))
+  (simple-stmt-pass)
+  (simple-stmt-break)
+  (simple-stmt-continue)
+  (simple-stmt-print-stmt 
+   (stmt (list-of atom-exp?))))
 
-(define-datatype func_param* func_param*?
-  (empty-param)
-  (func_params (param func_param?) (rest-params func_param*?))
-  )
+(define-datatype compound-stmt compound-stmt?
+  (compound-stmt-function-def 
+   (stmt function-def?))
+  (compound-stmt-if-stmt 
+   (stmt if-stmt?))
+  (compound-stmt-for-stmt 
+   (stmt for-stmt?)))
+
+(define-datatype assignments assignments?
+  (assignment-stmt 
+   (id symbol?)
+   (exp1 expression?)))
+
+(define-datatype return-stmt return-stmt?
+  (void-return-stmt)
+  (value-return-stmt
+   (exp1 expression?)))
+
+(define-datatype global-stmt global-stmt?
+  (global-stmt1 
+   (id symbol?)))
+
+(define-datatype function-def function-def?
+  (function-def-with-params
+   (id symbol?)
+   (params params?)
+   (stmts statements?)
+   (saved-env environment?))
+  (funcion-def-without-params
+   (id symbol?)
+   (stmts statements?)
+   (saved-env environment?)))
+
+(define-datatype params params?
+  (param 
+   (params params-with-default?))
+  (params1
+   (params-exp params?)
+   (param-exp params-with-default?)))
+
+(define-datatype params-with-default params-with-default?
+  (param-with-default
+   (id symbol?)
+   (exp1 expression?)))
+
+(define-datatype if-stmt if-stmt?
+  (if-stmt1
+   (exp1 expression?)
+   (stmts statements?)
+   (else-block else-block?)))
+
+(define-datatype else-block else-block?
+  (else-block1
+   (stmts statements?)))
+
+(define-datatype for-stmt for-stmt?
+  (for-stmt1 
+   (id symbol?)
+   (exp1 expression?)
+   (stmts statements?)))
 
 (define-datatype expression expression?
-  (binary_op (op procedure?) (left expression?) (right expression?))
-  (unary_op (op procedure?) (operand expression?))
-  (function_call (func expression?) (params expression*?))
-  (list_ref (ref expression?) (index expression?))
-  (ref (var string?))
+  (expression1
+   (exp1 disjunction-exp?)))
 
-  (atomic_bool_exp (bool boolean?))
-  (atomic_num_exp (num number?))
-  (atomic_null_exp)
-  (atomic_list_exp (l expression*?))
-  )
+(define-datatype disjunction-exp disjunction-exp?
+  (cnj-disj
+   (exp conjunction-exp?))
+  (or-exp
+   (dis-exp disjunction-exp?)
+   (con-exp conjunction-exp?)))
 
-(define-datatype expression* expression*?
-  (empty-expr)
-  (expressions (expr expression?) (rest-exprs expression*?))
-  )
+(define-datatype conjunction-exp conjunction-exp?
+  (inv-conj
+   (exp inversion-exp?))
+  (and-exp
+   (con-exp conjunction-exp?)
+   (inv-exp inversion-exp?)))
 
+(define-datatype inversion-exp inversion-exp?
+  (not-exp
+   (inv-exp inversion-exp?))
+  (comparison-inv
+   (cmp-exp comparison-exp?)))
+
+(define-datatype comparison-exp comparison-exp?
+  (eq-sum-comparison
+   (exp eq-sum-exp?))
+  (lt-sum-comparison
+   (exp lt-sum-exp?))
+  (gt-sum-comparison
+   (exp gt-sum-exp?))
+  (sum-comparison
+   (exp sum-exp?)))
+
+(define-datatype eq-sum-exp eq-sum-exp?
+  (eq
+   (exp1 sum-exp?)
+   (exp2 sum-exp?)))
+
+(define-datatype lt-sum-exp lt-sum-exp?
+  (lt
+   (exp1 sum-exp?)
+   (exp2 sum-exp?)))
+
+(define-datatype gt-sum-exp gt-sum-exp?
+  (gt
+   (exp1 sum-exp?)
+   (exp2 sum-exp?)))
+
+(define-datatype sum-exp sum-exp?
+  (sum
+   (exp1 sum-exp?)
+   (exp2 term-exp?))
+  (minus 
+   (exp1 sum-exp?)
+   (exp2 term-exp?))
+  (term-sum
+   (exp1 term-exp?)))
+
+(define-datatype term-exp term-exp?
+  (mul-term 
+   (exp1 term-exp?)
+   (exp2 factor-exp?))
+  (div-term 
+   (exp1 term-exp?)
+   (exp2 factor-exp?))
+  (term
+   (exp1 factor-exp?)))
+
+(define-datatype factor-exp factor-exp?
+  (plus-factor 
+   (exp1 power-exp?))
+  (minus-factor 
+   (exp1 power-exp?))
+  (power-factor 
+   (exp1 power-exp?)))
+
+(define-datatype power-exp power-exp?
+  (power1
+   (atom atom-exp?)
+   (factor factor-exp?))
+  (primary-power
+   (exp1 primary-exp?)))
+
+(define-datatype primary-exp primary-exp?
+  (atom-primary
+   (exp1 atom-exp?))
+  (array-primary
+   (prim primary-exp?)
+   (exp expression?))
+  (callable-primary 
+   (prim primary-exp?))
+  (callable-primary-with-args
+   (prim primary-exp?)
+   (args arguments-exp?)))
+
+(define-datatype arguments-exp arguments-exp?
+  (arguments1
+   (exp1 expression?))
+  (arguments2 
+   (args arguments-exp?)
+   (exp1 expression?)))
+
+(define-datatype atom-exp atom-exp?
+  (id-atom
+   (id symbol?))
+  (bool-atom
+   (val boolean?))
+  (none-atom)
+  (num-atom
+   (val number?))
+  (list-atom
+   (val list-exp?)))
+
+(define-datatype list-exp list-exp?
+  (list1
+   (exps expressions?))
+  (empty-list))
+
+(define-datatype expressions expressions?
+  (exps
+   (exps expressions?)
+   (exp1 expression?))
+  (a-exp
+   (exp1 expression?)))
+
+;Add all values
+(define-datatype expval expval?
+  (num-val
+   (num number?))
+  (empty-val))
+
+(define-datatype environment environment?
+  (empty-environment)
+  (extend-environment
+   (var string?)
+   (val expval?)
+   (env environment)))
 
 (provide (all-defined-out))
 (#%provide (all-defined))
