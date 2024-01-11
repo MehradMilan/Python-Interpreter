144a6fbe3c31be588e52ca00296c960da22e8a41
Ali Rahmizad <79265219+AliAgha888@users.noreply.github.com>
2024-01-11 19:53:01 +0330

    add value-of: program, statements, statement

--- interpreter.rkt
+++ interpreter.rkt
@@ -6,3 +6,20 @@
 #lang racket
 
 (require (lib "eopl.ss" "eopl"))
+
+(define value-of-program
+  (lambda (_program)
+    (cases program _program)
+      (prog (_statements) (value-of-statements _statements))))
+
+(define value-of-statements
+  (lambda (_statements)
+    (cases statements _statements)
+      (stmt (_statement) (value-of-statement _statement))
+      (stmts (_statements _statement) (begin (value-of-statements _statements) (value-of-statement _statement)))))
+
+(define value-of-statement
+  (lambda (_statement)
+    (cases statement _statement)
+      (stmt-compound-stmt (_compound-stmt) (value-of-compound-stmt _compound-stmt))
+      (stmt-simple-stmt (_simple-stmt) (value-of-simple-stmt _simple-stmt))))
