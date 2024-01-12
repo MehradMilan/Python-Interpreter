4c11c38e7125820d1380a52ae6441e909dbcac46
AliSafara <ali.safarafard@gmail.com>
2024-01-12 13:29:36 +0330

    we love you ali jan

--- passes/interpreter.rkt
+++ passes/interpreter.rkt
@@ -10,22 +10,6 @@
 (require "../datatypes.rkt")
 
 
-;;; (define value-of-program
-;;;   (lambda (_program)
-;;;     (cases program _program)
-;;;       (prog (_statements) (value-of-statements _statements))))
-
-;;; (define value-of-statements
-;;;   (lambda (_statements)
-;;;     (cases statements _statements)
-;;;       (stmt (_statement) (value-of-statement _statement))
-;;;       (stmts (_statements _statement) (begin (value-of-statements _statements) (value-of-statement _statement)))))
-
-;;; (define value-of-statement
-;;;   (lambda (_statement)
-;;;     (cases statement _statement)
-;;;       (stmt-compound-stmt (_compound-stmt) (value-of-compound-stmt _compound-stmt))
-;;;       (stmt-simple-stmt (_simple-stmt) (value-of-simple-stmt _simple-stmt))))
 
 (define run 
     (lambda (abstract-syntax-tree)
